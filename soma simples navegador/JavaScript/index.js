94c5f0ff7c41d18ebc6d4f139238091d0bd9df03
Matheus Afonso <94645126+Matheus-A-Afonso@users.noreply.github.com>
2022-01-29 17:35:15 -0300

    Add files via upload

---
+++ soma simples navegador/JavaScript/index.js
@@ -0,0 +1,14 @@
+alert ('To te vendo'); // função do navegador
+
+let num1 = prompt ('Digite um número');
+let num2 = prompt ('Outro numero');
+
+num1 = Number (num1);
+num2 = Number (num2);
+
+console.log (num1+num2);
+
+const resultado = (num1+num2);
+alert (`o resultado da sua conta foi: ${resultado}`); // poderia ser feito diretamente com a expressão dessa forma
+
+// convertendo prompt para número 
